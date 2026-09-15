ccacea7130ce93207a614f81a08667e24da5784e
soponv692-bit <soponv692@gmail.com>
2026-09-15 22:15:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import edge_tts
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, ContextTypes, filters
 
-BOT_TOKEN = "8942545300:AAFoQnIkoC4A982dIpTGrXfwgaBw9UsKviU"
+BOT_TOKEN = "8942545300:AAFoQnlKoC4A982dIpTGrXfWgaBw9UsKviU".strip()
 
 user_voices = {}
 
